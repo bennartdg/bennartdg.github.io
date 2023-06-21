9cb561cebdcd366a0455a1042c67353425cd0569
Bennart Gunawan <bennart.dgunawan@gmail.com>
2023-06-21 13:29:12 +0700

    creating repository

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const navbar = document.getElementsByTagName("nav")[0];
+window.addEventListener("scroll", function () {
+  console.log(window.scrollY);
+  if (this.window.scrollY > 1) {
+    navbar.classList.replace("nav-transparent", "nav-color");
+  } else if (window.scrollY <= 0) {
+    navbar.classList.replace("nav-color", "nav-transparent");
+  }
+});
+
+const scrollIndicator = document.querySelector('.scroll-indicator');
+
+function update() {
+  scrollIndicator.style.width = `${((window.scrollY) / (document.body.scrollHeight - window.innerHeight) * 100 )}%`
+  requestAnimationFrame(update);
+}
+
+update();
